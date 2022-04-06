5c97794d2920792e5e310f6d9f687567ff82b023
mitsuhamiyamizu477 <100654798+mitsuhamiyamizu477@users.noreply.github.com>
2022-04-06 11:17:40 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+figlet -c After Effects CS3 Installer
+echo "$(tput setaf 2)================================================$(tput sgr 0)"
+echo "$(tput setaf 2)SCRIPT CREATED BY SAKURA YUI CH.$(tput sgr 0)"
+echo "$(tput setaf 2)================================================$(tput sgr 0)"
+echo "$(tput setaf 0)❀ $(tput setaf 2)Subscribe To My YT: https://bit.ly/3u4gpbQ $(tput sgr 0)"
+echo "$(tput setaf 0)❀ $(tput setaf 2)Follow Me On FB: https://bit.ly/3LAPF8U $(tput sgr 0)"
+echo "$(tput setaf 2)================================================$(tput sgr 0)"
+echo "$(tput setaf 2) $(tput sgr 0)"
+echo "$(tput setaf 2)Script is now starting... $(tput sgr 0)"
+echo "$(tput setaf 2) $(tput sgr 0)"
+echo "$(tput setaf 2)================================================$(tput sgr 0)"
+echo "$(tput setaf 2)Enabling 32bit packages and library support... $(tput sgr 0)"
+echo "$(tput setaf 2)================================================$(tput sgr 0)"
